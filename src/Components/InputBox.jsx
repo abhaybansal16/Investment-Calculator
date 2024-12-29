9490307abf3e3a6a6b8adf3634d77cf3e7675a5e
abhaybansal16 <bansalabhay081@gmail.com>
2024-12-29 17:43:27 +0530

    non negative input

--- src/Components/InputBox.jsx
+++ src/Components/InputBox.jsx
@@ -6,21 +6,21 @@ export default function InputBox({ onSelect, Value })
             <div className="input-group">
                 <p>
                     <label>Initial Investment</label>
-                    <input type="number" value= {Value.initialInvestment} onChange = {(event) => onSelect('initialInvestment',event.target.value)} required/>
+                    <input type="number" min={0} value= {Value.initialInvestment} onChange = {(event) => onSelect('initialInvestment',event.target.value)} required/>
                 </p>
                 <p>
                     <label>Annual Investment</label>
-                    <input type="number" value= {Value.annualInvestment} onChange = {(event) => onSelect('annualInvestment',event.target.value)} required/>
+                    <input type="number" min={0} value= {Value.annualInvestment} onChange = {(event) => onSelect('annualInvestment',event.target.value)} required/>
                 </p>
             </div>
             <div className="input-group">
                 <p>
                     <label>Expected Return</label>
-                    <input type="number" value= {Value.expectedReturn} onChange = {(event) => onSelect('expectedReturn',event.target.value)} required/>
+                    <input type="number" min={0} value= {Value.expectedReturn} onChange = {(event) => onSelect('expectedReturn',event.target.value)} required/>
                 </p>
                 <p>
                     <label>Duration</label>
-                    <input type="number" value= {Value.duration} onChange = {(event) => onSelect('duration',event.target.value)} required/>
+                    <input type="number" min={1} value= {Value.duration} onChange = {(event) => onSelect('duration',event.target.value)} required/>
                 </p>
             </div>
         </section>
